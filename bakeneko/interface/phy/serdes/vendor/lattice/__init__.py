8da873f76758aa1b6d5ffd15f63de751492e3cae
Aki Van Ness <aki@lethalbit.net>
2025-08-19 14:55:17 -0700

    bakeneko.interface.phy.lattice: Bubbled up ECP5 SerDes

--- bakeneko/interface/phy/serdes/vendor/lattice/__init__.py
+++ bakeneko/interface/phy/serdes/vendor/lattice/__init__.py
@@ -4,7 +4,8 @@
 Lattice Semiconductor device-specific SerDes and PIPE Interfaces.
 '''
 
+from .ecp5 import ECP5SerDesPIPE
 
 __all__ = (
-
+	'ECP5SerDesPIPE',
 )
--- bakeneko/interface/phy/serdes/vendor/lattice/ecp5/__init__.py
+++ bakeneko/interface/phy/serdes/vendor/lattice/ecp5/__init__.py
@@ -4,5 +4,14 @@
 Lattice Semiconductor ECP5/ECP5-5G SerDes Interfaces
 '''
 
+from .dcu  import DCU
+from .pipe import ECP5SerDesPIPE
+from .sci  import SCI, CHRegister, DCURegister
+
 __all__ = (
+	'DCU',
+	'ECP5SerDesPIPE',
+	'SCI',
+	'CHRegister',
+	'DCURegister',
 )
