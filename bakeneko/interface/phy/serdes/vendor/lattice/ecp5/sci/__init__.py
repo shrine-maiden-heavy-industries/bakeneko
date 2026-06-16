345c58ee26ca786639b7b5375aeffbdd7c830978
Aki Van Ness <aki@lethalbit.net>
2026-06-16 12:19:25 -0700

    phy.serdes.vendor.lattice: Stubbed out some of the initial SCI machinery for the ECP5 DCUs

--- bakeneko/interface/phy/serdes/vendor/lattice/ecp5/sci/__init__.py
+++ bakeneko/interface/phy/serdes/vendor/lattice/ecp5/sci/__init__.py
@@ -8,6 +8,7 @@ and it's channels.
 '''
 
 from torii.build.plat import Platform
+from torii.hdl.ast    import Signal
 from torii.hdl.dsl    import Module
 from torii.hdl.ir     import Elaboratable
 
@@ -29,9 +30,47 @@ class SCI(Elaboratable):
 	'''
 
 	def __init__(self) -> None:
-		pass
+		self.dcu_sel = Signal()
+		self.ch_sel  = Signal()
+		self.re      = Signal()
+		self.we      = Signal()
+		self.done    = Signal()
+		self.addr    = Signal(6)
+		self.data_w  = Signal(8)
+		self.data_r  = Signal(8)
+
+		self.sci_rd     = Signal()
+		self.sci_wr_n   = Signal()
+		self.sci_addr   = Signal(6)
+		self.sci_data_w = Signal(8)
+		self.sci_data_r = Signal(8)
 
 	def elaborate(self, platform: Platform | None) -> Module:
 		m = Module()
+
+		m.d.comb += [
+			self.sci_wr_n.eq(1),
+			self.sci_addr.eq(self.addr),
+			self.sci_data_w.eq(self.data_w),
+		]
+
+		with m.FSM(domain = 'phy', name = 'DCU/SCI') as fsm:
+			m.d.comb += [ self.done.eq(fsm.ongoing('IDLE')), ]
+
+			with m.State('IDLE'):
+				with m.If(self.we):
+					m.next = 'WRITE'
+				with m.Elif(self.re):
+					m.next = 'READ'
+					m.d.comb += [ self.sci_rd.eq(1), ]
+
+			with m.State('WRITE'):
+				m.next = 'IDLE'
+				m.d.comb += [ self.sci_wr_n.eq(0), ]
+
+			with m.State('READ'):
+				m.next = 'IDLE'
+				m.d.comb += [ self.sci_rd.eq(1), ]
+				m.d.phy += [ self.data_r.eq(self.sci_data_r), ]
 
 		return m
